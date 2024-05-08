3ba334b2386ee023de09a8d2a4112004408dfc43
Thanh Dau <hello.thanhdauxuan@gmail.com>
2024-05-09 00:28:03 +0700

    Bundle backend to docker image

--- backend/flask_run.py
+++ backend/flask_run.py
@@ -1,4 +1,11 @@
 from flask_app import app
+import logging
 
 if __name__ == '__main__':
    app.run()
+
+# Logging in Gunicorn server
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
