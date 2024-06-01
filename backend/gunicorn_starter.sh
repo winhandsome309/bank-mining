753bceb35f38e9fb6afec496d51bc030c3f57821
Thanh Dau <hello.thanhdauxuan@gmail.com>
2024-06-01 12:09:24 +0700

    Update

--- backend/gunicorn_starter.sh
+++ backend/gunicorn_starter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Here we will be spinning up multiple threads with multiple worker processess(-w) and perform a binding.
-gunicorn flask_run:"app" --log-level=debug -w 3 --threads 3 -b 0.0.0.0:8000
+gunicorn flask_run:"app" --log-level=debug -w 1 --threads 60 -b 0.0.0.0:8000
